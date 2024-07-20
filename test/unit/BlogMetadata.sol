a42b060bb0caa6e08f2a864b0b229103b9bb8e07
zkFriendly <zkfriendly@proton.me>
2024-07-20 12:30:40 +0200

    refactor: call methods directly on blog contracts

--- test/unit/BlogMetadata.sol
+++ test/unit/BlogMetadata.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 import {SemaphoreFactory} from "src/SemaphoreFactory.sol";
 import {SemaphoreFactoryTest} from "test/unit/utils.sol";
 import {ISemaphore} from "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
+import {ISemaphoreBlog} from "src/interfaces/ISemaphoreBlog.sol";
 
 contract CreateAndJoinTest is SemaphoreFactoryTest {
     uint256 alicePk = 0xA11CE;
@@ -13,32 +14,32 @@ contract CreateAndJoinTest is SemaphoreFactoryTest {
     function test_group_creator_should_be_able_to_set_metadata_uri() public {
         uint256 groupId = 1;
         string memory metadataUri = "ipfs://QmXyZ";
-        _createGroup(groupId, alice);
+        address blog = _createGroup(groupId, alice);
         vm.prank(alice);
-        factory.setMetadataUri(groupId, metadataUri);
-        string memory updatedMetadataUri = factory.metadataUri(groupId);
+        ISemaphoreBlog(blog).setMetadataUri(metadataUri);
+        string memory updatedMetadataUri = ISemaphoreBlog(blog).metadataUri();
         assertEq(updatedMetadataUri, metadataUri);
     }
 
     function test_non_creator_should_not_be_able_to_set_metadata_uri() public {
         uint256 groupId = 1;
         string memory metadataUri = "ipfs://QmXyZ";
-        _createGroup(groupId, alice);
+        address blog = _createGroup(groupId, alice);
         vm.prank(bob);
-        vm.expectRevert(SemaphoreFactory.OnlyCreator.selector);
-        factory.setMetadataUri(groupId, metadataUri);
+        vm.expectRevert(ISemaphoreBlog.OnlyCreator.selector);
+        ISemaphoreBlog(blog).setMetadataUri(metadataUri);
     }
 
     function test_group_creator_should_be_able_to_update_metadata_uri() public {
         uint256 groupId = 1;
         string memory metadataUri = "ipfs://QmXyZ";
-        _createGroup(groupId, alice);
+        address blog = _createGroup(groupId, alice);
         vm.prank(alice);
-        factory.setMetadataUri(groupId, metadataUri);
+        ISemaphoreBlog(blog).setMetadataUri(metadataUri);
         string memory updatedMetadataUri = "ipfs://QmUpdated";
         vm.prank(alice);
-        factory.setMetadataUri(groupId, updatedMetadataUri);
-        string memory newMetadataUri = factory.metadataUri(groupId);
+        ISemaphoreBlog(blog).setMetadataUri(updatedMetadataUri);
+        string memory newMetadataUri = ISemaphoreBlog(blog).metadataUri();
         assertEq(newMetadataUri, updatedMetadataUri);
     }
 
@@ -47,14 +48,14 @@ contract CreateAndJoinTest is SemaphoreFactoryTest {
         uint256 groupId2 = 2;
         string memory metadataUri1 = "ipfs://QmXyZ";
         string memory metadataUri2 = "ipfs://QmAbC";
-        _createGroup(groupId1, alice);
-        _createGroup(groupId2, alice);
+        address blog1 = _createGroup(groupId1, alice);
+        address blog2 = _createGroup(groupId2, bob);
         vm.prank(alice);
-        factory.setMetadataUri(groupId1, metadataUri1);
-        vm.prank(alice);
-        factory.setMetadataUri(groupId2, metadataUri2);
-        string memory updatedMetadataUri1 = factory.metadataUri(groupId1);
-        string memory updatedMetadataUri2 = factory.metadataUri(groupId2);
+        ISemaphoreBlog(blog1).setMetadataUri(metadataUri1);
+        vm.prank(bob);
+        ISemaphoreBlog(blog2).setMetadataUri(metadataUri2);
+        string memory updatedMetadataUri1 = ISemaphoreBlog(blog1).metadataUri();
+        string memory updatedMetadataUri2 = ISemaphoreBlog(blog2).metadataUri();
         assertEq(updatedMetadataUri1, metadataUri1);
         assertEq(updatedMetadataUri2, metadataUri2);
     }
